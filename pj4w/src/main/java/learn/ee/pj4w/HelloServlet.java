7ef0a83c829193a2fec63e8fd31cf57c2e0f0780
Denis Popkov <pkvdenis@gmail.com>
2020-08-13 23:51:13 +0300

    pj4w: ch3: parameters and forms, MultiValueParameterServlet

--- pj4w/src/main/java/learn/ee/pj4w/HelloServlet.java
+++ pj4w/src/main/java/learn/ee/pj4w/HelloServlet.java
@@ -1,12 +1,20 @@
 package learn.ee.pj4w;
 
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Date;
+import java.io.PrintWriter;
 
+@WebServlet(
+        name = "helloServlet",
+        urlPatterns = {"/greeting", "/hello", "/salutation", "/wazzup"},
+        loadOnStartup = 1
+)
 public class HelloServlet extends HttpServlet {
+    private static final String DEFAULT_USER = "Guest";
+
     @Override
     public void init() {
         System.out.println("Servlet " + this.getServletName() + " has started.");
@@ -14,7 +22,32 @@ public class HelloServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        resp.getWriter().println("Greetings from " + this.getServletName() + "! Today is " + new Date() + ".");
+        String user = req.getParameter("user");
+        if (user == null) {
+            user = DEFAULT_USER;
+        }
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding("UTF-8");
+        PrintWriter writer = resp.getWriter();
+        writer.append("<!DOCTYPE html>\r\n")
+                .append("<html>\r\n")
+                .append("   <head>\r\n")
+                .append("       <title>Hello User Application</title>\r\n")
+                .append("   </head>\r\n")
+                .append("   <body>\r\n")
+                .append("       Hello, ").append(user).append("!<br/><br/>\r\n")
+                .append("       <form action=\"greeting\" method=\"POST\">\r\n")
+                .append("           Enter your name:<br/>\r\n")
+                .append("           <input type=\"text\" name=\"user\"/><br/>\r\n")
+                .append("           <input type=\"submit\" value=\"Submit\"/>\r\n")
+                .append("       </form>\r\n")
+                .append("   </body>\r\n")
+                .append("</html>\r\n");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        doGet(req, resp);
     }
 
     @Override
--- pj4w/src/main/webapp/index.jsp
+++ pj4w/src/main/webapp/index.jsp
@@ -3,6 +3,7 @@
 <h2>PJ4W</h2>
 
 <p>HelloServlet: <a href="${pageContext.request.contextPath}/hello">Hello</a> </p>
+<p>MultiValueParameterServlet: <a href="${pageContext.request.contextPath}/checkboxes">Checkboxes</a> </p>
 
 </body>
 </html>
