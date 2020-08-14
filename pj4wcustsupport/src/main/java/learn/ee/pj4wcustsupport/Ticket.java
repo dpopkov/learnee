1832b2691650d6a91fa917fec1f3e87589e39223
Denis Popkov <pkvdenis@gmail.com>
2020-08-14 18:34:25 +0300

    pj4wcustsupport: add project, list and create tickets, upload files

---
+++ pj4wcustsupport/src/main/java/learn/ee/pj4wcustsupport/Ticket.java
@@ -0,0 +1,52 @@
+package learn.ee.pj4wcustsupport;
+
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class Ticket {
+    private String customerName;
+    private String subject;
+    private String body;
+    private final Map<String, Attachment> attachments = new LinkedHashMap<>();
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public Attachment getAttachment(String name) {
+        return attachments.get(name);
+    }
+
+    public Collection<Attachment> getAttachments() {
+        return attachments.values();
+    }
+
+    public void addAttachment(Attachment attachment) {
+        attachments.put(attachment.getName(), attachment);
+    }
+
+    public int getNumberOfAttachments() {
+        return attachments.size();
+    }
+}
---
+++ pj4wcustsupport/src/main/webapp/index.jsp
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <title>Index</title>
+</head>
+<body>
+
+<h2>Index</h2>
+
+<p><a href="${pageContext.request.contextPath}/tickets">View All Tickets</a></p>
+
+</body>
+</html>
